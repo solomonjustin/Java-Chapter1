fe1e270d082a9df19cea5e4ded6c93ca54fa3c37
solomonjustin <solomon.justinm@gmail.com>
2016-01-11 09:45:28 -0600

    Revert "Revert "Commit Lab1""
    This reverts commit e0a6fcaf1b9bfc9ce591213916090066a7c72daa.

---
+++ Lab1.java
@@ -0,0 +1,16 @@
+//This program will store my age, name, a desired annual income as literals
+//It will also dispay these values on screen
+
+
+public class Lab1
+{
+	public static void main(String[] args)
+	{
+		//vars that will be used in the income profile
+		String name = "Justin Solomon";
+		int age = 20;
+		int desiredIncome = 150000;
+
+		System.out.printf("My name is %s, my age is %d, and I hope to earn $%,.2d a year.\n", name, age, desiredIncome);
+	}
+}
